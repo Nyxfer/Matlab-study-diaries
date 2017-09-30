afa4ab28d5f28afae2be21206209015031169b7b
Nyxfer <nyx_flower@outlook.com>
2017-09-30 22:20:36 +0100

    30/09/17

---
+++ Essential Matlab for Engineers and Scientists/input-statement.m
@@ -0,0 +1,10 @@
+balance = input('Enter bank balance:');
+rate = input('Enter interest rate:');
+interest = rate * balance;
+
+balance = balance + interest;
+
+format bank
+
+disp('New balance:');
+disp( balance );
